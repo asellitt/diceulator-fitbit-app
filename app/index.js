c98f3779d670237faf659317ec682233a7867631
Anthony Sellitti <anthony.sellitti@envato.com>
2020-03-11 21:42:55 +1100

    onactivate is a buggy piece of shit that fires twice in the context of a panoramic view.

--- app/index.js
+++ app/index.js
@@ -3,32 +3,32 @@ import * as util from "../common/utils"
 
 const equationText = document.getElementById("equation")
 const calculatorResultText = document.getElementById("calculatorResult")
-document.getElementById("1").onactivate = (event) => { util.buildEquation(1, equationText) }
-document.getElementById("2").onactivate = (event) => { util.buildEquation(2, equationText) }
-document.getElementById("3").onactivate = (event) => { util.buildEquation(3, equationText) }
-document.getElementById("4").onactivate = (event) => { util.buildEquation(4, equationText) }
-document.getElementById("5").onactivate = (event) => { util.buildEquation(5, equationText) }
-document.getElementById("6").onactivate = (event) => { util.buildEquation(6, equationText) }
-document.getElementById("7").onactivate = (event) => { util.buildEquation(7, equationText) }
-document.getElementById("8").onactivate = (event) => { util.buildEquation(8, equationText) }
-document.getElementById("9").onactivate = (event) => { util.buildEquation(9, equationText) }
-document.getElementById("0").onactivate = (event) => { util.buildEquation(0, equationText) }
-document.getElementById("d").onactivate = (event) => { util.buildEquation('d', equationText) }
-document.getElementById("plus").onactivate = (event) => { util.buildEquation('+', equationText) }
-document.getElementById("minus").onactivate = (event) => { util.buildEquation('-', equationText) }
-document.getElementById("delete").onactivate = (event) => { util.buildEquation('del', equationText) }
-document.getElementById("roll").onactivate = (event) => { util.solveEquation(equationText, calculatorResultText) }
-document.getElementById("clear").onactivate = (event) => {
+document.getElementById("1").onclick = (event) => { util.buildEquation(1, equationText) }
+document.getElementById("2").onclick = (event) => { util.buildEquation(2, equationText) }
+document.getElementById("3").onclick = (event) => { util.buildEquation(3, equationText) }
+document.getElementById("4").onclick = (event) => { util.buildEquation(4, equationText) }
+document.getElementById("5").onclick = (event) => { util.buildEquation(5, equationText) }
+document.getElementById("6").onclick = (event) => { util.buildEquation(6, equationText) }
+document.getElementById("7").onclick = (event) => { util.buildEquation(7, equationText) }
+document.getElementById("8").onclick = (event) => { util.buildEquation(8, equationText) }
+document.getElementById("9").onclick = (event) => { util.buildEquation(9, equationText) }
+document.getElementById("0").onclick = (event) => { util.buildEquation(0, equationText) }
+document.getElementById("d").onclick = (event) => { util.buildEquation('d', equationText) }
+document.getElementById("plus").onclick = (event) => { util.buildEquation('+', equationText) }
+document.getElementById("minus").onclick = (event) => { util.buildEquation('-', equationText) }
+document.getElementById("delete").onclick = (event) => { util.buildEquation('del', equationText) }
+document.getElementById("roll").onclick = (event) => { util.solveEquation(equationText, calculatorResultText) }
+document.getElementById("clear").onclick = (event) => {
   equationText.text = ''
   calculatorResultText.text = ''
 }
 
 const diceResultText = document.getElementById("diceResult")
-document.getElementById("d2").onactivate = (event) => { diceResultText.text = util.d(1, 2) }
-document.getElementById("d4").onactivate = (event) => { diceResultText.text = util.d(1, 4) }
-document.getElementById("d6").onactivate = (event) => { diceResultText.text = util.d(1, 6) }
-document.getElementById("d8").onactivate = (event) => { diceResultText.text = util.d(1, 8) }
-document.getElementById("d10").onactivate = (event) => { diceResultText.text = util.d(1, 10) }
-document.getElementById("d00").onactivate = (event) => { diceResultText.text = util.d(1, 100) }
-document.getElementById("d12").onactivate = (event) => { diceResultText.text = util.d(1, 12) }
-document.getElementById("d20").onactivate = (event) => { diceResultText.text = util.d(1, 20) }
+document.getElementById("d2").onclick = (event) => { diceResultText.text = util.d(1, 2) }
+document.getElementById("d4").onclick = (event) => { diceResultText.text = util.d(1, 4) }
+document.getElementById("d6").onclick = (event) => { diceResultText.text = util.d(1, 6) }
+document.getElementById("d8").onclick = (event) => { diceResultText.text = util.d(1, 8) }
+document.getElementById("d10").onclick = (event) => { diceResultText.text = util.d(1, 10) }
+document.getElementById("d00").onclick = (event) => { diceResultText.text = util.d(1, 100) }
+document.getElementById("d12").onclick = (event) => { diceResultText.text = util.d(1, 12) }
+document.getElementById("d20").onclick = (event) => { diceResultText.text = util.d(1, 20) }
